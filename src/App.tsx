7aac4c891f77ac6443a001c834b8d8fb4ec63070
Annarchive <ytpt@mail.ru>
2023-06-13 23:42:36 +0400

    Developed app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 import './App.css';
+import AddPizzaForm from "./components/AddPizzaForm";
+import Pizza from "./modals/Pizza";
+import DisplayPizzas from "./components/DisplayPizzas";
 
+const App: FC = () => {
 
-function App() {
+    const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
+
+    const addPizza = (newPizza: Pizza) => {
+        setPizzasList([...pizzasList, newPizza]);
+    }
+
+    const updatePizza = (newPizza: Pizza) => {
+        setPizzasList(pizzasList.map((pizza) =>
+            (pizza.id === newPizza.id ? newPizza : pizza)));
+    }
+
+    const deletePizza = (id: number) => {
+        const newPizzasList = pizzasList.filter(pizza => pizza.id !== id);
+        setPizzasList(newPizzasList);
+    }
+
+  console.log(pizzasList);
+
   return (
     <div className="App">
-      New App!
+        <div className="wrap">
+            <span className="heading">MAKE LOVE PIZZA</span>
+            <AddPizzaForm addPizza={addPizza} />
+            <DisplayPizzas
+                pizzasList={pizzasList}
+                updatePizza={updatePizza}
+                deletePizza={deletePizza}
+            />
+        </div>
     </div>
   );
 }
---
+++ src/components/DisplayPizzas.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+import Pizza from "../modals/Pizza";
+import SinglePizza from "./SinglePizza";
+
+interface DisplayPizzasProps {
+    pizzasList: Pizza[];
+    updatePizza: (newPizza: Pizza) => void;
+    deletePizza: (id: number) => void;
+}
+
+const DisplayPizzas: FC<DisplayPizzasProps> = ({ pizzasList, updatePizza, deletePizza }) => {
+    return (
+        <div className="container">
+            { pizzasList.map((pizza) => {
+                return <SinglePizza
+                    key={pizza.id}
+                    pizza={pizza}
+                    updatePizza={updatePizza}
+                    deletePizza={deletePizza}
+                />
+            }) }
+        </div>
+    )
+}
+
+export default DisplayPizzas;
